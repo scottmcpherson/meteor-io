3059d07980145f53a2053270853894e590581763
scott-mcpherson <scott_fl@me.com>
2015-08-25 08:51:01 -0700

    Load environment variables

---
+++ lib/templates/templates/env.sh
@@ -0,0 +1,6 @@
+# IF you use the ion command to run your app
+# these environment variables will automatically be
+# imported into process.evn in your local environment.
+
+# Example:
+# MAIL_URL="smtp://username:password@smtp-url:port"
