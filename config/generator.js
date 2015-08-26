d1563642d1eb1ccb91ddcb251150ae92ef839329
scott-mcpherson <scott_fl@me.com>
2015-08-26 09:03:38 -0700

    Bump version to 0.5.1

--- config/generator.js
+++ config/generator.js
@@ -1,14 +1,19 @@
 var Command = require('{{commandPath}}');
+var path = require('path');
 
-class {{upperCaseName}} extends Command {
+class {{className}} extends Command {
   constructor () {
     super();
+    // A shorter name to call your generator with
+    this.alias = '{{alias}}';
+    // This name should match the file name without the extension
     this.name = '{{name}}';
-    this.usage = 'ion g:{{name}}';
+    // A brief description on how to use
+    this.description = 'io g:{{name}} <name>, io g:{{alias}} <name>';
   }
-  run () {
+  run (args, argv) {
     // Generator code here
   }
 }
 
-module.exports = new {{upperCaseName}};
+module.exports = new {{className}};
